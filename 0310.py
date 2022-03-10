13be0ea737ad432098fbfcd53207c333ad63621c
HyeSungE <101119619+HyeSungE@users.noreply.github.com>
2022-03-10 23:47:32 +0900

    Add files via upload
    20220310_python_study

---
+++ 0310.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('a')
+
+
+# In[2]:
+
+
+print(3+4)
+a=7
+print(a)
+print(a+3)
+print(a*3)
+
+
+# In[3]:
+
+
+print('a','b')
+print('c')
+print('d')
+
+
+# In[6]:
+
+
+print(1,2,sep=",")
+
+
+# In[7]:
+
+
+print(1,2,sep="")
+
+
+# In[8]:
+
+
+print(1,2,sep="->")
+
+
+# In[9]:
+
+
+print("고양이")
+print("강아지")
+print("고양이",end="")
+print("강아지")
+
+
+# In[10]:
+
+
+height=input('사용자의 키 : ')
+print(height)
+
+
+# In[11]:
+
+
+height=input('사용자의 키 : ')
+print(height)
+print(int(height)*2)
+
+
+# In[12]:
+
+
+height=int(input('사용자의 키 : '))
+print(height*2)
+
+
+# In[15]:
+
+
+name=input('이름을 입력하세요 : ')
+age=int(input('나이를 입력하세요 : '))
+print(name,'님은',age,'살입니다.')
+
+
+# In[16]:
+
+
+score=1
+print(score)
+score='A'
+print(score)
+
+
+# In[17]:
+
+
+score=1
+print(score,type(score),sep='  ')
+score='A'
+print(score,type(score),sep='  ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
